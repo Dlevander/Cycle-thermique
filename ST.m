37ef1bfff5f5a6406b464865d155c2178fb576c9
matmaes <38102410+matmaes@users.noreply.github.com>
2018-11-04 23:29:27 +0100

    Update ST.m
    rajout des états 70 et 80

--- ST.m
+++ ST.m
@@ -12,24 +12,24 @@ function [ETA XMASSFLOW DATEN DATEX DAT MASSFLOW COMBUSTION FIG] = ST(P_e,option
 % OPTIONS is a structure containing :
 %   -options.nsout     [-] : Number of feed-heating 
 %   -options.reheat    [-] : Number of reheating
-%   -options.T_max     [°C] : Maximum steam temperature
-%   -options.T_cond_out[°C] : Condenseur cold outlet temperature
+%   -options.T_max     [Â°C] : Maximum steam temperature
+%   -options.T_cond_out[Â°C] : Condenseur cold outlet temperature
 %   -options.p3_hp     [bar] : Maximum pressure
 %   -options.drumFlag  [-] : if =1 then drum if =0 => no drum. 
 %   -options.eta_mec   [-] : mecanic efficiency of shafts bearings
 %   -options.comb is a structure containing combustion data : 
-%       -comb.Tmax     [°C] : maximum combustion temperature
+%       -comb.Tmax     [Â°C] : maximum combustion temperature
 %       -comb.lambda   [-] : air excess
 %       -comb.x        [-] : the ratio O_x/C. Example 0.05 in CH_1.2O_0.05
 %       -comb.y        [-] : the ratio H_y/C. Example 1.2 in CH_1.2O_0.05
-%   -options.T_exhaust [°C] : Temperature of exhaust gas out of the chimney
+%   -options.T_exhaust [Â°C] : Temperature of exhaust gas out of the chimney
 %   -options.p_3       [-] : High pressure after last reheating
 %   -options.x4        [-] : Vapor ratio [gaseous/liquid] (in french : titre)
-%   -options.T_0       [°C] : Reference temperature
-%   -options.TpinchSub [°C] : Temperature pinch at the subcooler
-%   -options.TpinchEx  [°C] : Temperature pinch at a heat exchanger
-%   -options.TpinchCond[°C] : Temperature pinch at condenser 
-%   -options.Tdrum     [°C] : minimal drum temperature
+%   -options.T_0       [Â°C] : Reference temperature
+%   -options.TpinchSub [Â°C] : Temperature pinch at the subcooler
+%   -options.TpinchEx  [Â°C] : Temperature pinch at a heat exchanger
+%   -options.TpinchCond[Â°C] : Temperature pinch at condenser 
+%   -options.Tdrum     [Â°C] : minimal drum temperature
 %   -option.eta_SiC    [-] : Isotrenpic efficiency for compression
 %   -option.eta_SiT    [-] : Isotrenpic efficiency for Turbine. It can be a vector of 2 values :
 %             	             eta_SiT(1)=eta_SiT_HP,eta_SiT(2)=eta_SiT_others
@@ -64,7 +64,7 @@ function [ETA XMASSFLOW DATEN DATEX DAT MASSFLOW COMBUSTION FIG] = ST(P_e,option
 %   -datex(6) : perte_chemex [kW]
 %   -datex(7) : perte_transex[kW]
 % DAT is a matrix containing :
-% dat = {T_1       , T_2       , ...       , T_6_I,     T_6_II, ... ;  [°C]
+% dat = {T_1       , T_2       , ...       , T_6_I,     T_6_II, ... ;  [Â°C]
 %        p_1       , p_2       , ...       , p_6_I,     p_6_II, ... ;  [bar]
 %        h_1       , h_2       , ...       , h_6_I,     h_6_II, ... ;  [kJ/kg]
 %        s_1       , s_2       , ...       , s_6_I,     s_6_II, ... ;  [kJ/kg/K]
@@ -106,7 +106,7 @@ if nargin<3
     if nargin<2
         options = struct();
         if nargin<1
-            P_e = 250e3; % [kW] Puissance énergétique de l'installation
+            P_e = 250e3; % [kW] Puissance Ã©nergÃ©tique de l'installation
         end
     end
 end
@@ -129,13 +129,13 @@ end
 if isfield(options,'T_max')
     T_max = options.T_max;
 else
-    T_max = 525.0;  % [éC]
+    T_max = 525.0;  % [Ã©C]
 end
 
 if isfield(options,'T_cond_out')
      T_cond_out = options.T_cond_out;
 else
-     T_cond_out = 30.0 ;  % [éC]
+     T_cond_out = 30.0 ;  % [Ã©C]
 end
 
 if isfield(options,'p3_hp')
@@ -143,7 +143,7 @@ if isfield(options,'p3_hp')
 else
     p3_hp = 200;  % [bar]
 end
-%présence ou non d'un tiroir pour le superheater
+%prÃ©sence ou non d'un tiroir pour le superheater
 if isfield(options,'drumFlag')
      drumFlag = options.drumFlag;
 else
@@ -160,7 +160,7 @@ if isfield(options,'comb')
     if isfield(options.comb,'Tmax')
         Tmax = options.comb.Tmax;
     else
-        Tmax = 500;  % [éC] A MODIFIER
+        Tmax = 500;  % [Ã©C] A MODIFIER
     end
     if isfield(options.comb,'lambda')
         lambda = options.comb.lambda;
@@ -178,7 +178,7 @@ if isfield(options,'comb')
         y = 4;  % [-] CH4
     end
 else
-    Tmax = 500;  % [éC] A MODIFIER
+    Tmax = 500;  % [Ã©C] A MODIFIER
     lambda = 1.05;  % [-]
     x = 0;  % [-] CH4
     y = 4;  % [-] CH4
@@ -187,7 +187,7 @@ end
 if isfield(options,'T_exhaust')
      T_exhaust = options.T_exhaust;
 else
-     T_exhaust = 120.0;  % [éC]
+     T_exhaust = 120.0;  % [Ã©C]
 end
 
 if isfield(options,'p_3')
@@ -205,25 +205,25 @@ end
 if isfield(options,'T_0')
     T_60 = options.T_0;
 else
-    T_60 = 15.0;  % [éC]
+    T_60 = 15.0;  % [Ã©C]
 end
 
 if isfield(options,'TpinchSub')
      TpinchSub= options.TpinchSub;
 else
-     TpinchSub = 115.0;  % [éC]
+     TpinchSub = 115.0;  % [Ã©C]
 end
 
 if isfield(options,'TpinchEx')
      TpinchEx = options.TpinchEx;
 else
-     TpinchEx = 490.0;  % [éC]
+     TpinchEx = 490.0;  % [Ã©C]
 end
 
 if isfield(options,'TpinchCond')
      TpinchCond= options.TpinchCond;
 else
-     TpinchCond = 18.0;  % [éC]
+     TpinchCond = 18.0;  % [Ã©C]
 end
 
 if isfield(options,'Tdrum')
@@ -244,7 +244,7 @@ if isfield(options,'eta_SiT')
      
 else
      eta_SiT_HP = 0.9;  % [-] A MODIFIER , PLUSIEURS VALEURS ? DIFFERENTS PAR TURBINE ?
-     eta_SiT_others = 0.92; % on considère rendement égale pour les turbines MP et BP
+     eta_SiT_others = 0.92; % on considÃ¨re rendement Ã©gale pour les turbines MP et BP
 end
 
 if P_e == null 
@@ -276,12 +276,12 @@ DAT= zeros(6,20);
 
 %%%%%%%%%%%%%%% ETAT 30 %%%%%%%%%%%%%%% 
 
-% Sortie chaudière
+% Sortie chaudiÃ¨re
 T_30 = T_max;
 p_30 = p3_hp;
 h_30 = XSteam('h_pT',p_30,T_30);
 s_30 = XSteam('s_pT',p_30,T_30);
-%x_30 = XSteam('x_ph',p_30,h_30); = 1 car vapeur surchauffée
+%x_30 = XSteam('x_ph',p_30,h_30); = 1 car vapeur surchauffÃ©e
 e_30 = exergie(h_30,s_30);
 
 
@@ -302,17 +302,17 @@ if reheat == 1
     e_40 = exergie(h_40,s_40);
     
     %%%%%%%%%%%%%%% ETAT 50 %%%%%%%%%%%%%%%
-    % Entrée de la turbine MP dans le cas d'une resurchauffe
+    % EntrÃ©e de la turbine MP dans le cas d'une resurchauffe
     
     p_50 = p_3; %la pression apres la resurchauffe
     T_50 = T_30;
     h_50 = XSteam('h_pT',p_50,T_50);
     s_50 = XSteam('s_pT',p_50,T_50);
-    %x_50 = XSteam('x_ph',p_50,h_50); = 1 car vapeur surchauffée
+    %x_50 = XSteam('x_ph',p_50,h_50); = 1 car vapeur surchauffÃ©e
     e_50 = exergie(h_50,s_50);
     
     %%%%%%%%%%%%%%% ETAT 60 %%%%%%%%%%%%%%%
-    % Sortie de la turbine BP dans cas isentropique (60s) et réel (60)
+    % Sortie de la turbine BP dans cas isentropique (60s) et rÃ©el (60)
     s_60s = s_50;
     T_60s = T_cond_out + TpinchCond;
     T_60 = T_cond_out + TpinchCond;
@@ -328,7 +328,7 @@ if reheat == 1
 %%%%%%%%%%%%%%%% Si pas resurchauffe %%%%%%%%%%%%%%%
 elseif reheat == 0
     %%%%%%%%%%%%%%% ETAT 60S + 60  %%%%%%%%%%%%%%%
-    % Sortie de la turbine BP (60) dans cas isentropique (60s) et réel (60)
+    % Sortie de la turbine BP (60) dans cas isentropique (60s) et rÃ©el (60)
     
     s_60s = s_30;
     T_60s = T_cond_out + TpinchCond;
@@ -347,6 +347,37 @@ elseif reheat == 0
 end
 
 
+    %%%%%%%%%%%%%%% ETAT 70 %%%%%%%%%%%%%%%
+    %Sortie du condenseur, liquide saturÃ©
+    T_70 = T_60; %car passe simplement de vapeur a liquide saturÃ©
+    h_70 = XSteam('hL_T',T_70);
+    p_70 = XSteam('psat_T',T_70);
+    s_70 = XSteam('sL_T',T_70);
+    e_70 = exergie(h_70,s_70);
+    %x_70=XSteam('x_ph',p_70,h_70); %=0
+    
+if nsout==0
+    %S'il n'y a pas de soutirage le point 1 = point 7 car on coupe avant Pe et on revient au point 1 avant Pa
+    T_10 = T_70;
+    h_10 = h_70;
+    p_10 = p_70;
+    s_10 = s_70;
+    e_10 = e_70;
+    %x_10 = x_70; %=0
+    
+elseif nsout>0
+    %S'il y a un ou plusieurs soutirages
+    
+    %%%%%%%%%%%%%%% ETAT 80 %%%%%%%%%%%%%%%
+    %AprÃ¨s la pompe Pe, de rendement isentropique option.eta_SiC
+    p_80 = ??? %j'ai demandÃ© Ã  l'assistant
+    h_is = XSteam('h_ps',p_80,s_70); %h dans le cas isentropique
+    h_80=h_70+option.eta_SiC*(h_is-h_70);%h en prenant compte rendement is
+    s_80=XSteam('s_ph',p_80,h_80);
+    T_80=XSteam('T_ph',p_80,h_80);
+    x_80=XSteam('x_ph',p_80,h_80);
+    e_80=exergie(h_80,s_80);
+    
 
 
 end
