c7885bce67706a5c6b3c59b27098373f4f126e85
Dlevander <44058672+Dlevander@users.noreply.github.com>
2018-12-05 15:05:53 +0100

    Update exergie.m

--- exergie.m
+++ exergie.m
@@ -1,6 +1,6 @@
-function [e] = exergie(h,s);
+function [e] = exergie(h,s)
 T0 = 15;
 h0 = XSteam('hL_T',T0);
 s0 = XSteam('sL_T',T0);
-e =(h-h0)-(T0+273.15)*(s-s0);
+e =(h-h0)-(T0+273.15).*(s-s0);
 end
