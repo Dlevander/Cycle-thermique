2ef642710b1374b6981f57a7ff0933516dd395c0
Dlevander <44058672+Dlevander@users.noreply.github.com>
2018-12-05 15:06:35 +0100

    Update reheat1nsout8.m

--- reheat1nsout8.m
+++ reheat1nsout8.m
@@ -8,7 +8,6 @@ close all
 % - eta_mec
 % - eta_SiC
 % - eta_SiT
-% - 
 
 optionsST.nsout = 8;   %  [-] : Number of feed-heating
 optionsST.reheat  = 1; %  [-] : Number of reheating
@@ -29,12 +28,12 @@ optionsST.p_3 = 62; %      [-] : High pressure after last reheating
 optionsST.TpinchSub = 4;%[°C] : Temperature pinch at the subcooler
 optionsST.TpinchEx = 15; %  [°C] : Temperature pinch at a heat exchanger
 optionsST.TpinchCond = 15; % [°C] : Temperature pinch at condenser
-%   -options.Tdrum     [°C] : minimal drum temperature
+optionsST.Tdrum = 120; %    [°C] : minimal drum temperature
 optionsST.eta_SiC = 0.85;%   [-] : Isotrenpic efficiency for compression
 optionsST.eta_SiT = 0.89;   %[-] : Isotrenpic efficiency for Turbine. It can be a vector of 2 values :
 %             	             eta_SiT(1)=eta_SiT_HP,eta_SiT(2)=eta_SiT_others
 
-P_e = 288e3;
+P_e = 288e3; %[kW]
 
-[ETA,XMASSFLOW,DATEN,DATEX,DAT,MASSFLOW,COMBUSTION,FIG] = ST(P_e,optionsST,1)
+[ETA,XMASSFLOW,DATEN,DATEX,DAT,MASSFLOW,COMBUSTION,FIG] = ST(P_e,optionsST,1);
 
