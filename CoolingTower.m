75edad7592a19a797039824ccbbf503e0a22003c
Dlevander <44058672+Dlevander@users.noreply.github.com>
2018-12-13 17:52:11 +0100

    Cooling tower debuggé  copletement
    A voir si on change les données du cas de base

--- CoolingTower.m
+++ CoolingTower.m
@@ -50,72 +50,69 @@ function [DAT_WATER DAT_AIR MASSFLOW] = CoolingTower(P_w,options)
 
 %% YOUR WORK
 if nargin<2
-    if isfield(P_w)
-        P_w = options.P_w;
-    else
-        P_w = 4; %[K]
-    end
-    
-    if isfield(options,'Tcond')
-        Tcond = options.Tcond;
-    else
-        Tcond = 4; %[K]
-    end
-    
-    if isfield(options,'Tpinch')
-        Tpinch = options.Tpinch;
-    else
-        Tpinch = 4; %[K]
-    end
-    
-    if isfield(options,'Tw_out')
-        Tw_out = options.Tw_out;
-    else
-        Tw_out = 4; %[K]
-    end
-    
-    if isfield(options,'Tw_in')
-        Tw_in = options.Tw_in;
-    else
-        Tw_in = 4; %[K]
-    end
-    
-    if isfield(options,'Triver')
-        Triver = options.Triver;
-    else
-        Triver = 4; %[K]
-    end
-    
-    if isfield(options,'Ta_in')
-        Ta_in = options.Ta_in;
-    else
-        Ta_in = 4; %[K]
-    end
-    
-    if isfield(options,'Ta_out')
-        Ta_out = options.Ta_out;
-    else
-        Ta_out = 4; %[K]
-    end
-    
-    if isfield(options,'Phi_atm')
-        Phi_atm = options.Phi_atm;
-    else
-        Phi_atm = 0.8; %[K]
-    end
-    
-    if isfield(options,'Phi_out')
-        Phi_out = options.Phi_out;
-    else
-        % minimisation du debit d'air necessaire avec
-        Phi_out = 1; %[K]
+    options = struct();
+    if nargin<1
+        P_w = 444e3; %[kW]
     end
 end
 
-Cp_el = 4.186e3; % Cp eau liquide suppose constant
+if isfield(options,'Tcond')
+    Tcond = options.Tcond;
+else
+    Tcond = 30; %[C]
+end
+
+if isfield(options,'Tpinch')
+    Tpinch = options.Tpinch;
+else
+    Tpinch = 4; %[C]
+end
+
+if isfield(options,'Tw_out')
+    Tw_out = options.Tw_out;
+else
+    Tw_out = 42.8; %[C]
+end
+
+if isfield(options,'Tw_in')
+    Tw_in = options.Tw_in;
+else
+    Tw_in = 4; %[C]
+end
+
+if isfield(options,'Triver')
+    Triver = options.Triver;
+else
+    Triver = 15; %[C]
+end
+
+if isfield(options,'Ta_in')
+    Ta_in = options.Ta_in;
+else
+    Ta_in = 15; %[C]
+end
+
+if isfield(options,'Ta_out')
+    Ta_out = options.Ta_out;
+else
+    Ta_out = 25; %[C]
+end
+
+if isfield(options,'Phi_atm')
+    Phi_atm = options.Phi_atm;
+else
+    Phi_atm = 0.8; %[K]
+end
+
+if isfield(options,'Phi_out')
+    Phi_out = options.Phi_out;
+else
+    % minimisation du debit d'air necessaire avec
+    Phi_out = 1; %[K]
+end
+Cp_el = 4.186; % [kj/kg/K] Cp eau liquide suppose constant
 %% etat a_in : air at the cooling tower inlet
 
-%Ta_in donnee 
 psat_a_in = XSteam('psat_T',Ta_in);
 p_a_in = 1.01325; %pression atm
 phia_in = Phi_atm;
@@ -125,8 +122,7 @@ ha_in = 1.009*(Ta_in+273.15) + xa_in*(2501.6 + 1.854*(Ta_in+273.15));
 %% etat a_out : air at the cooling tower outlet
 
 phia_out = Phi_out;
-%Ta_out donnee
-psat_a_out = XSteam('psat_T',Ta_out+273.15);
+psat_a_out = XSteam('psat_T',Ta_out);
 p_a_out = 1.01325; %pression atm
 xa_out = 0.622*phia_out*psat_a_out/(p_a_out-phia_out*psat_a_out);
 ha_out = 1.009*(Ta_out+273.15) + xa_out*(2501.6 + 1.854*(Ta_out+273.15));
@@ -134,24 +130,21 @@ ha_out = 1.009*(Ta_out+273.15) + xa_out*(2501.6 + 1.854*(Ta_out+273.15));
 %% etat e3 : water just after  condenser
 
 T_e3 = Tw_out;
-%Cp_e3 = janaf('c','H2O',T_e3+273.15); %[kj/kg/K]
 h_e3 = Cp_el*(T_e3+273.15); % [kj/kg]
 %% etat e2 : water just before condenser
 
 T_e2 = Tw_in;
-%Cp_e2 = janaf('c','H2O',T_e2+273.15); %[kj/kg/K]
 h_e2 = Cp_el*(T_e2+273.15); % [kj/kg]
 %% etat e4 : water from the river (coming between 1 & 2)
 
 T_e4 = Triver;
-%Cp_e4 = janaf('c','H2O',T_e4+273.15); %[kj/kg/K]
 h_e4 = Cp_el*(T_e4+273.15); % [kj/kg]
 
 %% debits
 
 m_as = P_w/(ha_out-ha_in);
 m_ev = m_as*(xa_out-xa_in);
-m_e = P_w/(Cp_e3*(T_e3+273.15) - Cp_e2*(T_e2+273.15));
+m_e = P_w/(Cp_el*(T_e3-T_e2));
 
 m_e1 = m_e-m_ev;
 m_e2 = m_e;
@@ -163,6 +156,7 @@ T_e1 = (m_e*Cp_el*(T_e3+273.15)-m_as*(ha_out-ha_in))/(m_e1*Cp_el);
 h_e1 = Cp_el*(T_e1+273.15);
 
 MASSFLOW = [m_e,m_ev,m_as];
+
 DAT_WATER = [T_e1       , T_e2       , T_e3       , T_e4;  %[C]
              h_e1       , h_e2       , h_e3       , h_e4;  %[kJ/kg]
              m_e1       , m_e2       , m_e3       , m_e4]; %[kg/s]
