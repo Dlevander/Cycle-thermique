4d469981be414ba7c95e3d297a0c3324bdd2a15b
Dlevander <44058672+Dlevander@users.noreply.github.com>
2018-12-13 22:24:29 +0100

    Update plot_GT.m

--- plot_GT.m
+++ plot_GT.m
@@ -3,6 +3,7 @@ function [FIG] = plot_GT(DAT,eta_PiT,eta_PiC,R_air)
     linP12 = linespace(DAT(2,1),DAT(2,2),100);
     linr12 = p_2/fliplr(linP12);
     linT12 = transf_poly('compression',DAT(1,1),linr12,eta_PiC,R_air,0,0,0,0);
+    linT12K = 
     linS12 = DAT(4,1) + (1-eta_PiC)*Cp_12*log((linT12+273.15)/(DAT(1,1)+273.15));
     %Combustion isobare 2-3
     TK_2 = DAT(1,2)+273.15;
@@ -17,7 +18,13 @@ function [FIG] = plot_GT(DAT,eta_PiT,eta_PiC,R_air)
     linP34 = linespace(DAT(2,3),DAT(2,4),100);
     linr34 = p_4/fliplr(linP34);
     linT34 = transf_poly('detente',DAT(1,3),linr34,eta_PiT,R_fum,x_CO2,x_H2O,x_O2,x_N2);
-    linS34 = DAT(4,3) - Cp_34*log(linT34/TK_3)* ((1-eta_PiT)/eta_PiT);
+    linT34K = linT34+273.15;
+    linS34 = DAT(4,3) - Cp_34*log(linT34K/TK_3)* ((1-eta_PiT)/eta_PiT);
     %Plot
+    FIG = figure;
     plot([linS12 linS23 linS34],[linT12 linT23 linT34]);
+    grid on
+    title('Diagramme T-s')
+    xlabel('s [kj/kg K]')
+    ylabel('T [°C]')
 end
