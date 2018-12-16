30852e3bda4c73033e0c7d8e8ae4aac1d3c910d8
matmaes <38102410+matmaes@users.noreply.github.com>
2018-12-16 22:29:04 +0100

    Update combustion.m

--- combustion.m
+++ combustion.m
@@ -18,7 +18,7 @@ ma1 = (32+3.76*28.15)*(1+(y/4))/(12.01+1.008*y); % pouvoir comburivore [kg_air_s
 if lambda==0
 cp_CH4 = 2.44*1000; %cp methane a 15C, trouve sur internet J/kg*K
 b2 = y/2; %pg 26 cours combu
-a2 = 1; % ???
+a2 = 1; % 
 b1 = 0; %hypothese
 a1 = 0;
 
@@ -33,8 +33,6 @@ Num = (b2/2+a2)*hO2_3*Mm_O2 - b2*hH2O_3*Mm_H2O - a2*hCO2_3*Mm_CO2 + LHV_mol*1000
 Denom = (hO2_3*Mm_O2+ 3.76*hN2_3*Mm_N2- hO2_2*Mm_O2- 3.76*hN2_2*Mm_N2);
 w = Num/Denom; %w, coefficient stoech de l'air
 lambda = w/(1+y/4); %X=0 pour nous
-
-% lambda = 2.2864 ; % valeur qu'il faut atteindre (GT) pour l'exemple bouquin p125 !!!
 end
 
 %%determination des fractions massiques des fumees
@@ -82,26 +80,3 @@ x_H2O = x_H2O/Sum;
 ma1 = (32+3.76*28.15)*(1+(y/4))/(12.01+1.008*y); % pouvoir comburivore [kg_air_stoech/kg_comb]
 R_fum = 8.314/Mm_fum;
 end
-
-% % temperature de l'air aux reactifs = T2
-% Tr=linspace(273,T2);
-% cp_air_R = (0.21*32/28.94*janaf('c','O2',T2+273.15)+janaf('c','N2',T2+273.15)*0.79*28/28.94)*1000;
-%  
-% % temperature des produits = T3
-% cp_air_P = (0.21*32/28.94*janaf('c','O2',T3+273.15)+janaf('c','N2',T3+273.15)*0.79*28/28.94)*1000;
-% cp_CO2 =janaf('c','CO2',T3+273.15)*1000;
-% cp_N2 = janaf('c','N2',T3+273.15)*1000;
-% cp_H2O = janaf('c','H2O',T3+273.15)*1000;
-% cp_O2 = janaf('c','O2',T3+273.15)*1000;
-% lambda = fzero(@(L) (LHV*1000+cp_CH4*100 + L*ma1*cp_air_R*T2)/(1+L*ma1)-(44*cp_CO2+18*2*cp_H2O+(L-1)*2*cp_O2*32+3.76*L*2*cp_N2*28)*T3./(44+18*2+(L-1)*(1+y/4)*32+3.76*L*(1+y/4)*28) ,3);%T3-273-(cp_CH4*(25)+L*ma1*cp_air_R*(T2-273)+1000*PCI)/(44/16*cp_CO2+18/16*2*cp_H2O+ma1*(L-1)*cp_air_P+ma1*cp_N2*28/28.94)
-
-%w = (803.3*1000-hCO2_3+2*hO2_2-2*hH2O_3+2440*15)/(hO2_3+3.76*hN2_3-0.21*h
-% solution = fsolve(@solver,[0 0 0]);
-% w = solution(1);
-% a0 = solution(2);
-% lambda = solution(3);
-% function Solve = solver(s) % Solve = [w a0 lam fm]
-% Solve(1) = 2-x-2*s(1)+(y/2)+2*s(2); % stoechimoetric balance for combustion p128
-% Solve(2) = s(1)/(1+(y-2*x)/4)-s(3) ; % definition of air excess coefficient
-% Solve(3) = (hCO2_3+(y/2)*hH2O_3)+s(2)*hO2_3+3.76*s(1)*hN2_3-LHV*1000-Cp_CH4*15-s(1)*(hO2_2+3.76*hN2_2); % energy balance for combustion
-% end
