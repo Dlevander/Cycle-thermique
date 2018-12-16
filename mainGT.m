e5daa5ee2c7138e385c1ea8c43efb2dfa3ffc707
matmaes <38102410+matmaes@users.noreply.github.com>
2018-12-16 17:19:22 +0100

    Update mainGT.m

--- mainGT.m
+++ mainGT.m
@@ -1,11 +1,11 @@
 function [ ETA DATEN DATEX DAT MASSFLOW COMBUSTION ] = mainGT(P_e,options,display )
-P_e = 225000; %kW
+P_e = 230000; %kW
 options.k_mec =0.015;
 options.T_0 =15;
 options.T_ext = 15;
-options.r   = 10;
+options.r   = 18;
 options.k_cc = 0.95;
-options.T_3 = 1050;
+options.T_3 = 1400;
 option.eta_PiC =0.9;
 option.eta_PiT =0.9;
 
