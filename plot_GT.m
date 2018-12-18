68c205a97ef078bbeb7b80aa7f80880d6e6a82ef
Dlevander <44058672+Dlevander@users.noreply.github.com>
2018-12-18 15:42:45 +0100

    correction pression p23 plot

--- plot_GT.m
+++ plot_GT.m
@@ -9,13 +9,6 @@ function [FIG] = plot_GT(DAT,eta_PiT,eta_PiC,x,y,k_cc)
     linr12 = linP12./DAT(2,1);
     linT12 = arrayfun(@(r) transf_poly('compression',DAT(1,1),r,eta_PiC,R_air,0,0,0,0),linr12);
     linT12K =linT12+273.15;
-%     for i=1:length(linT12K)
-%         if linT12K(i)<300
-%             linT12Kjanaf(i) = 300;
-%         else
-%             linT12Kjanaf(i) = linT12K(i);
-%         end
-%     end
     Cp_12 = arrayfun(@(t) (x_a_N2*mean(janaf('c','N2',linspace(300,t)))+x_a_O2*mean(janaf('c','O2',linspace(300,t)))),linT12K) ;
     %Cp_12 =(arrayfun(@(t) CP(x_a_O2,0,0,x_a_N2,[300 t]),linT12K)); %[kj/kg/K]
     linS12 = DAT(4,1) + (1-eta_PiC).*Cp_12.*log((linT12K)./(DAT(1,1)+273.15));%[kj/kg/K]
@@ -23,7 +16,7 @@ function [FIG] = plot_GT(DAT,eta_PiT,eta_PiC,x,y,k_cc)
     TK_2 = DAT(1,2)+273.15;
     linT23 = linspace(DAT(1,2),DAT(1,3),100);
     linT23K = linT23+273.15;
-    p_3 = DAT(2,2)*k_cc; %pertes de charges dans chambre combustion
+    linP23= linspace(DAT(2,2),DAT(2,3),100); %pertes de charges dans chambre combustion
     %preallocation
     x_N2 = zeros(1,length(linT23));
     x_O2 = zeros(1,length(linT23));
@@ -31,6 +24,7 @@ function [FIG] = plot_GT(DAT,eta_PiT,eta_PiC,x,y,k_cc)
     x_H2O = zeros(1,length(linT23));
     R_fum = zeros(1,length(linT23));
     Cp_23 = zeros(1,length(linT23));
+    linS23 = zeros(1,length(linT23));
     linT34= zeros(1,length(linT23));
     Cp_34 = zeros(1,length(linT23));
     %Detente 3-4
@@ -42,14 +36,13 @@ function [FIG] = plot_GT(DAT,eta_PiT,eta_PiC,x,y,k_cc)
         %Combustion isobare 2-3
         [x_N2(i),x_O2(i),x_CO2(i),x_H2O(i),R_fum(i),~,~,~,~] = combustion(x,y,DAT(1,2),linT23(i),0);
         Cp_23(i) = CP(x_O2(i),x_CO2(i),x_H2O(i),x_N2(i),[TK_2 linT23K(i)]);
+        linS23(i) = DAT(4,2)+Cp_23(i).*log(linT23K(i)./TK_2) - R_fum(i).*log(linP23(i)/DAT(2,2))*1e-3;
+        
         %Detente 3-4
         linT34(i) = transf_poly('detente',DAT(1,3),linr34(i),eta_PiT,R_fum(i),x_CO2(i),x_H2O(i),x_O2(i),x_N2(i));
         linT34K = linT34+273.15;
         Cp_34(i) = CP(x_O2(i),x_CO2(i),x_H2O(i),x_N2(i),[TK_3 linT34K(i)]);
     end
-    
-    %Combustion isobare 2-3
-    linS23 = DAT(4,2)+Cp_23.*log(linT23K./TK_2) - R_fum.*log(p_3/DAT(2,2))*1e-3;
     %Detente 3-4
     linS34 = DAT(4,3) - Cp_34.*log(linT34K./TK_3)* ((1-eta_PiT)/eta_PiT);
     
@@ -59,9 +52,8 @@ function [FIG] = plot_GT(DAT,eta_PiT,eta_PiC,x,y,k_cc)
     hold on 
     grid on
     plot(DAT(4,:),DAT(1,:),'*');
-    plot(linS12,linT12,'r')
-    plot(linS23,linT23,'r')
-    plot([DAT(4,3) linS34(2:length(linS34))],[DAT(1,3) linT34(2:length(linT34))],'r')
+    plot([linS12 linS23],[linT12 linT23],'r')
+    plot(linS34,linT34,'r')
     title('Diagramme T-s')
     xlabel('s [kj/kg/K]')
     ylabel('T [°C]')
